39565db3d58c0edb84a0dcaa37b4ff378287be67
Jameson <jctheathlete@gmail.com>
2017-08-16 04:58:54 +0000

    update dburl

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var express         = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
-    
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://jcheo:rusty@ds025802.mlab.com:25802/yelpcampjc");
+
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://jcheo:rusty@ds025802.mlab.com:25802/yelpcampjc");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
